0f23314b7b016e97faface1707c433b676f8f327
Joey Lee <jleepianist@gmail.com>
2017-05-04 23:38:56 -0700

    (feat-update) Implemented findSolution fn
    The findSolution higher-order function takes as arguments:
    1)the current row you're on
    2)n, or the total number of rows or columns of the boards
    3)the current board
    4)the validator fn (e.g. 'hasAnyRooksConflicts')
    5)callback fn

--- src/solvers.js
+++ src/solvers.js
@@ -6,15 +6,30 @@
 
 */
 
-// hint: you'll need to do a full-search of all possible arrangements of pieces!
-// (There are also optimizations that will allow you to skip a lot of the dead search space)
-// take a look at solversSpec.js to see what the tests are expecting
+// a higher-order full-search of all possible arrangements of pieces
+window.findSolution = function(row, n, board, validator, callback) {
+  // if all rows exhausted, this is a valid solution.
+  if (row === n) {
+    return callback();
+  }
 
+  // iterate over possible decisions
+  for (var i = 0; i < n; i++) {
+    // place a piece
+    board.togglePiece(row, i);
+    // recurse into remaining problem
+    if (!board[validator]()) {
+      var result = findSolution(row + 1, n, board, validator, callback);
+      if (result) {
+        return result; // EJECT
+      }
+    }
+    // unplace a piece
+    board.togglePiece(row, i);
+  }
+};
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
-
-
-
 window.findNRooksSolution = function(n) {
   var board = new Board({n: n});
 
